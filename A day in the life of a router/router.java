b85d2f34fbd269b4c4049bcab4db75df055f9414
kcanez <kevin.canez@gmail.com>
2014-06-23 19:56:32 -0700

    First Commit... dont judge this code

---
+++ A day in the life of a router/router.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+public class router
+{
+	public static void main(String args[])
+	{
+		Scanner scan = new Scanner(System.in);
+		int numNodes;
+		numNodes = scan.nextInt();
+		int[][] distanceMatrix = new int[numNodes][numNodes];
+		
+		for(int i = 0; i < numNodes; i++)
+		{
+			
+		}
+		
+		
+		
+		System.out.println(shortestPath(numNodes));
+	}
+	
+	public static String shortestPath(int n)
+	{
+		return "end";
+	}
+	
+}

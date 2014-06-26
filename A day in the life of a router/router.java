196ee22d9f0c8a468aa56b9fb5077451d51085af
kcanez <kevin.canez@gmail.com>
2014-06-25 21:37:33 -0700

    garbage

--- A day in the life of a router/router.java
+++ A day in the life of a router/router.java
@@ -31,19 +31,64 @@ public class router
 	
 	public static String shortestPath(int n, int[][] distMatrix, char start, char end)
 	{
-		int sNode = Character.getNumericValue(start) - 9;
-		int eNode = Character.getNumericValue(end) - 9;
+		int sNode = Character.getNumericValue(start) - 10;
+		int eNode = Character.getNumericValue(end) - 10;
 		char[][] prev = new char[n][n];
-		int[][] dist = new int[n][n];
+		double[][] dist = new double[n][n];
 		String shortPath = "";
+		String alphabet = "ABCDEFHIJKLMNOPQRSTUVWXYZ";
+		String tempList = alphabet.substring(0, n);
+		char[] nodeList = tempList.toCharArray();
+		char[] nodesPassed = new char[n];
+		boolean stillLooking = true;
 		
-		/*
-		 * Bed Time
-		 * To Do:
-		 * 
-		 * Impliment Dijkstra here
-		 * 
-		 */
+		//Dijkstra initialization
+		
+		// Wow, this code is garbage... scrap & restart time!
+		for(int i = 0; i < n; i++)
+		{
+			if(distMatrix[sNode][i] == -1)
+				dist[sNode][i] = Double.POSITIVE_INFINITY;
+			else
+			{
+				dist[sNode][i] = distMatrix[sNode][i];
+				prev[sNode][i] = start;
+			}
+		}
+		
+		
+		nodeList[sNode] = ' ';
+		nodesPassed[0] = start;
+		
+		int count = 0;
+		int minIndex = 0;
+		while(!stillLooking)
+		{
+			
+			for(int i = 0; i < n; i ++)
+			{
+				if(dist[count][minIndex] > dist[count][i])
+					minIndex = i;
+			}
+			count++;
+			
+			nodesPassed[count] = nodeList[minIndex];
+			nodeList[minIndex] = ' ';
+			
+			for(int i = 0; i < n; i++)
+			{
+				if((dist[count-1][i] > (dist[count-1][minIndex] + distMatrix[minIndex][i])) && distMatrix[minIndex][i] >= 0)
+				{
+					dist[count][i] = dist[count-1][minIndex] + distMatrix[minIndex][i];
+					prev[count][]
+				}
+				else
+				{
+					dist[count][i] = dist[count-1][i];
+				}
+			}
+			
+		}
 		
 		return Integer.toString(sNode);
 	}

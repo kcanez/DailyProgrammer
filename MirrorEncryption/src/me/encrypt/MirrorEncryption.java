b2d76a8721ac97e42f53d13cdfc87593b4705867
kcanez <kevin.canez@gmail.com>
2016-07-29 19:14:46 -0700

    pushing it out

--- MirrorEncryption/src/me/encrypt/MirrorEncryption.java
+++ MirrorEncryption/src/me/encrypt/MirrorEncryption.java
@@ -7,12 +7,31 @@ import java.io.IOException;
 public class MirrorEncryption 
 {
 	public static char[][] matrix = new char[15][15];
+	public static String top = "abcdefghijklm"; // top matrix[0][1]
+	public static String left = "ABCDEFGHIJKLM"; // left matrix[1][0]
+	public static String bottom = "NOPQRSTUVWXYZ";
+	public static String right = "nopqrstuvwxyz";
+	public static String startingLocation = "";
+	public static int[][] startingCoords = new int[1][4];
 
 	public static void main(String[] args) 
 	{
 		try 
 		{
 			getMatrix();
+			setStartingPoint('a');
+			System.out.println(startingCoords[0][0] + ", " + startingCoords[0][1] + " " + startingCoords[0][2] + ", " + startingCoords[0][3]);
+			setStartingPoint('b');
+			System.out.println(startingCoords[0][0] + ", " + startingCoords[0][1] + " " + startingCoords[0][2] + ", " + startingCoords[0][3]);
+			setStartingPoint('c');
+			System.out.println(startingCoords[0][0] + ", " + startingCoords[0][1] + " " + startingCoords[0][2] + ", " + startingCoords[0][3]);
+			setStartingPoint('n');
+			System.out.println(startingCoords[0][0] + ", " + startingCoords[0][1] + " " + startingCoords[0][2] + ", " + startingCoords[0][3]);
+			setStartingPoint('Z');
+			System.out.println(startingCoords[0][0] + ", " + startingCoords[0][1] + " " + startingCoords[0][2] + ", " + startingCoords[0][3]);
+			setStartingPoint(' ');
+			System.out.println(startingCoords[0][0] + ", " + startingCoords[0][1] + " " + startingCoords[0][2] + ", " + startingCoords[0][3]);
+			
 		} 
 		catch (IOException e) 
 		{
@@ -28,20 +47,63 @@ public class MirrorEncryption
 		return 'a';
 	}
 	
-	public static int findLetter(char charToFind)
+	public static char getLetter(int row, int col)
 	{
+		return matrix[row][col];
+	}
+	
+	public static void setStartingPoint(char charToFind)
+	{
+		int temp = charToFind;
 		
-		
-		return 5;
+		if(right.indexOf(charToFind) >= 0)
+		{
+			startingCoords[0][0] =  right.indexOf(charToFind)+1;
+			startingCoords[0][1] =  14;
+			startingCoords[0][2] = 0;
+			startingCoords[0][3] = -1;
+		}	
+		else if(top.indexOf(charToFind) >= 0)
+		{
+			startingCoords[0][0] =  0;
+			startingCoords[0][1] =  top.indexOf(charToFind)+1;
+			startingCoords[0][2] = 1;
+			startingCoords[0][3] = 0;
+		}
+		else if(bottom.indexOf(charToFind) >= 0)
+		{
+			startingCoords[0][0] =  14;
+			startingCoords[0][1] =  bottom.indexOf(charToFind)+1;
+			startingCoords[0][2] = -1;
+			startingCoords[0][3] = 0;
+		}
+		else if(left.indexOf(charToFind) >= 0)
+		{
+			startingCoords[0][0] =  left.indexOf(charToFind)+1;
+			startingCoords[0][1] =  0;
+			startingCoords[0][2] = 0;
+			startingCoords[0][3] = 1;
+		}
+		else if(temp == 32) // space
+		{
+			startingCoords[0][0] =  0;
+			startingCoords[0][1] =  0;
+			startingCoords[0][2] = 0;
+			startingCoords[0][3] = 0;
+		}
+		else
+		{
+			startingCoords[0][0] =  -1;
+			startingCoords[0][1] =  -1;
+			startingCoords[0][2] = 0;
+			startingCoords[0][3] = 0;
+		}
 	}
 	
 	
 	public static void getMatrix() throws IOException
 	{
-		String top = "abcdefghijklm"; // top matrix[0][1]
-		String left = "ABCDEFGHIJKLM"; // left matrix[1][0]
-		String bottom = "NOPQRSTUVWXYZ";
-		String right = "nopqrstuvwxyz";
+
 		int count = 1;
 		
 		while(count < 14)
@@ -66,16 +128,18 @@ public class MirrorEncryption
         
         while ((line = br.readLine()) != null) 
         {
-	            for(int i = 1; i < line.length();i++)
+	            for(int i = 1; i <= line.length();i++)
 	            {
-	            	matrix[j][i] = line.charAt(i);
+	            	matrix[j][i] = line.charAt(i-1);
+	            	System.out.println(i);
 	            }
 	            j++;
         }
 		
         br.close();
         
-		
+        matrix[0][0] = ' ';
+        
 		for(int i = 0; i < matrix.length; i++)
 		{
 			for(int y = 0; y < matrix.length;y++)
